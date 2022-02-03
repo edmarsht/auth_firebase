50331ee167f3ceeb55992610dd1bdf7170ea7e07
edmarsht <edtoulet@gmail.com>
2022-02-03 14:54:31 +0100

    create user and validations

--- src/context/userContext.js
+++ src/context/userContext.js
@@ -1,15 +1,31 @@
 import { createContext, useState, useEffect } from "react";
 
+import {
+    signInWithEmailAndPaswword, 
+    createUserWithEmailAndPassword, 
+    onAuthStateChanged
+} from "firebase/auth";
+import {auth} from "../firebase-config";
+
 // le contexte est crée ici 
 export const UserContext = createContext()
+
 
 // un nouveau composant d'ordre supérieur qui entoure tous les composants à qui on veut passer des infos 
 export function UserContextProvider(props) {
 
+    // Pour firebase auth 
+const [currentUser, setCurrentUser] = useState();
+const [loadingData, setLoadingData] = useState(true);
+
+const signUp = (email, pwd) => createUserWithEmailAndPassword(auth, email, pwd)
+
+
+
 const [test, setTest] = useState("ok")
 
 return (
-    <UserContext.Provider value={{test}}>
+    <UserContext.Provider value={{test, signUp}}>
         {props.children}
     </UserContext.Provider>
 
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,68 +1,101 @@
 import React, { useContext, useRef, useState } from "react";
-import {UserContext} from "../../context/userContext";
+import { UserContext } from "../../context/userContext";
 import "./Signup.css";
 
 function Signup() {
+  // test est la valeur que j'ai fait passer, j'utilise ensuite le hook useContext pour récupérer le context UserContext
+  const { test, signUp } = useContext(UserContext);
 
-    // test est la valeur que j'ai fait passer, j'utilise ensuite le hook useContext pour récupérer le context UserContext
-    const {test} = useContext(UserContext);
-    console.log(test)
+  const [validation, setValidation] = useState("");
 
-    const [validation, setValidation] = useState("");
+  const inputs = useRef([]);
+  const addInputs = (el) => {
+    // si el existe et qu'il n'est pas dans le tableau, je push el dans mon tableau inputs
+    if (el && !inputs.current.includes(el)) {
+      inputs.current.push(el);
+    }
+  };
+  const formRef = useRef();
+  const handleForm = async (e) => {
+    // Comportement par défault pour ne pas avoir le rafraichissement
+    e.preventDefault();
 
-    
-    const inputs = useRef([]);
-    const addInputs = el => {
-        // si el existe et qu'il n'est pas dans le tableau, je push el dans mon tableau inputs 
-        if(el && !inputs.current.includes(el)){
-            inputs.current.push(el)
-        }
+    if (
+      (inputs.current[1].value.length || inputs.current[2].value.length) < 6
+    ) {
+      setValidation("6 Characters min");
+      return;
+    } else if (inputs.current[1].value !== inputs.current[2].value) {
+      setValidation("Passwords do not match");
+      return;
     }
 
-    const handleForm = e => {
-        // Comportement par défault pour ne pas avoir le rafraichissement
-        e.preventDefault()
-        if((inputs.current[1].value.length || inputs.current[2].value.length) < 6) {
-            setValidation("6 Characters min")
-            return;
-        } else if(inputs.current[1].value !== inputs.current[2].value) {
-            setValidation("Passwords do not match")
-            return;
-        } 
+    try {
+      const cred = await signUp(
+        inputs.current[0].value,
+        inputs.current[1].value
+      )
+      // formRef sert à récupérer notre formulaire
+      // .reset() est une méthode JS qui permet de remettre à 0 les inputs
+      formRef.current.reset();
+      // Puis on supprime message d'erreur 
+      setValidation("");
+      console.log(cred)
+    } catch (err) {
+        if(err.code === "auth/invalid-email") {
+            setValidation("Email format invalid")
         }
-    
+        if(err.code === "auth/email-already-in-use") {
+            setValidation("Email already used")
+        }
+    }
+  };
 
   return (
     <div className="signup section__padding section__margin">
       <div className="signup__container">
         <h1>Sign Up</h1>
         <div className="signup__container-content">
-
-
-          <form onSubmit={handleForm}>
+          <form onSubmit={handleForm} ref={formRef}>
             <div className="input__section">
               <label htmlFor="signUpEmail">
                 <h3>Email</h3>
               </label>
-              <input ref={addInputs} name="email" type="email" required id="signUpEmail" />
+              <input
+                ref={addInputs}
+                name="email"
+                type="email"
+                required
+                id="signUpEmail"
+              />
             </div>
             <div className="input__section">
               <label htmlFor="signUpPassword">
                 <h3>Password</h3>
               </label>
-              <input ref={addInputs} name="password" type="password" required id="signUpPassword" />
+              <input
+                ref={addInputs}
+                name="password"
+                type="password"
+                required
+                id="signUpPassword"
+              />
             </div>
             <div className="input__section">
               <label htmlFor="signUpPasswordrepeat">
                 <h3>Confirm Password</h3>
               </label>
-              <input ref={addInputs} name="passwordrepeat" type="password" required id="signUpPasswordrepeat" />
+              <input
+                ref={addInputs}
+                name="passwordrepeat"
+                type="password"
+                required
+                id="signUpPasswordrepeat"
+              />
             </div>
             <button className="color__button">Submit</button>
             <h4 className="error__message">{validation}</h4>
           </form>
-
-        
         </div>
       </div>
     </div>
